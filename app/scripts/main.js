6e5fdbbbc88614a0f168a7322e9271135667e7b1
Felipe Maciel Ramos Vieira <felipemrvieira@gmail.com>
2019-06-21 17:29:58 -0300

    Cria parcialmente o componente de servicos

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,3 +7,29 @@ console.log('\'Allo \'Allo!');
 // Uncomment to enable Bootstrap popovers
 // https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
 // $(function () { $('[data-toggle="popover"]').popover(); });
+
+/*active button class onclick*/
+$('nav a').click(function(e) {
+  e.preventDefault();
+  $('nav a').removeClass('active');
+  $(this).addClass('active');
+  if(this.id === !'cidadao'){
+    $('.cidadao').addClass('noshow');
+  }
+  else if(this.id === 'cidadao') {
+    $('.cidadao').removeClass('noshow');
+    $('.rightbox').children().not('.cidadao').addClass('noshow');
+  }
+  else if (this.id === 'servidor') {
+    $('.servidor').removeClass('noshow');
+      $('.rightbox').children().not('.servidor').addClass('noshow');
+  }
+  else if(this.id === 'contribuinte') {
+    $('.contribuinte').removeClass('noshow');
+    $('.rightbox').children().not('.contribuinte').addClass('noshow');
+  }
+  else if(this.id === 'diario') {
+    $('.diario').removeClass('noshow');
+    $('.rightbox').children().not('.diario').addClass('noshow');
+  }
+});
